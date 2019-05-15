ca84f762a788d34f9cee8978dcdd15e427799b94
husseinadell <hassein1996@yahoo.com>
2019-05-15 10:05:27 +0200

    add basic ai random move with calculated cost

--- board.py
+++ board.py
@@ -1,18 +1,25 @@
+import numpy as np
+import random
+
+
 class BoardGame(object):
     def __init__(self, height=6, width=7):
         self.height = height
         self.width = width
-        self.board = [[0 for y in range(width)] for x in range(height)]
+        self.board = np.zeros((self.height, self.width))
         self.last_move = {"player": "NAN", "position": (10, 10)}
         self.Num_moves = 0
         self.game_over = False
         self.turn = 0
         return
 
-    def drop_piece(self, row, col, player):
-        self.board[row][col] = player
-        self.last_move["player"] = player
-        self.last_move["position"] = (row, col)
+    def drop_piece(self, row, col, player, temp_board=None):
+        if temp_board is None:
+            self.board[row][col] = player
+            self.last_move["player"] = player
+            self.last_move["position"] = (row, col)
+        else:
+            temp_board[row][col] = player
         # self.Num_moves += 1
 
     def is_valid_location(self, col):
@@ -48,10 +55,77 @@ class BoardGame(object):
                 if self.board[r][c] == self.board[r-1][c-1] == self.board[r-2][c-2] == self.board[r-3][c-3] == piece:
                     return True
 
+    def window_score(self, window, piece):
+        opp_piece = 1
+        score = 0
+        if window.count(piece) == 4:
+            score += 100
+        elif window.count(piece) == 3 and window.count(0) == 1:
+            score += 5
+        elif window.count(piece) == 2 and window.count(0) == 2:
+            score += 2
+
+        if window.count(opp_piece) == 3 and window.count(0) == 1:
+            score -= 4
+
+        return score
+
+    def score_position(self, temp_board, piece):
+        # score horizontal
+        score = 0
+        for r in range(self.height):
+            row_array = [int(i) for i in list(temp_board[r, :])]
+            for c in range(self.width-3):
+                window = row_array[c:c+4]
+                score += self.window_score(window, piece)
+        return score
+
+        # score vertical
+        for c in range(self.width):
+            col_array = [int(i) for i in list(temp_board[:, c])]
+            for r in range(self.height-3):
+                window = col_array[r:r+4]
+                score += self.window_score(window, piece)
+
+        # score positive diagonal
+        for r in range(self.height-3):
+            for c in range(self.width-3):
+                window = [temp_board[r+i][c+i] for i in range(3)]
+                score += self.window_score(window, piece)
+
+        # score negative diagonal
+        for r in range(self.height-3):
+            for c in range(self.width-3):
+                window = [temp_board[r+3-i][c+i] for i in range(3)]
+                score += self.window_score(window, piece)
+
+        return score
+
+    def get_valid_locations(self):
+        valid_locations = []
+        for col in range(self.width):
+            if self.is_valid_location(col):
+
+                valid_locations.append(col)
+        return valid_locations
+
+    def pick_best_move(self, piece):
+        valid_locations = self.get_valid_locations()
+        best_score = 0
+        best_col = random.choice(valid_locations)
+        for col in valid_locations:
+            row = self.get_next_open_row(col)
+            temp_board = self.board.copy()
+            self.drop_piece(row, col, piece, temp_board)
+            score = self.score_position(temp_board, piece)
+            if score > best_score:
+                best_score = score
+                best_col = col
+        return best_col
+
     def play(self):
         while not self.game_over:
-            for i in reversed(self.board):
-                print(i)
+            print(np.flip(self.board, 0))
             if self.turn == 0:
                 self.turn = 1
                 col = int(input("player 1 selection: "))
@@ -63,7 +137,7 @@ class BoardGame(object):
                         print("player 1 wins")
             else:
                 self.turn = 0
-                col = int(input("player 2 selection: "))
+                col = self.pick_best_move(2)
                 if self.is_valid_location(col):
                     row = self.get_next_open_row(col)
                     self.drop_piece(row, col, 2)
